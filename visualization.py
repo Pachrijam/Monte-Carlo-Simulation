28d4f4aaa228f69fd730595b2ccdd6a5b87aa72c
Pachrijam <paul830a@gmail.com>
2026-05-22 17:10:51 -0400

    improved visualization appearance

--- visualization.py
+++ visualization.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 from calculate import monte_carlo_pi
 
@@ -11,10 +12,14 @@ def visualizeOverTime(samples):
         xValues.append(num_samples)
         yValues.append(pi_estimate)
 
-    plt.plot(xValues, yValues)
+    plt.style.use('dark_background')
+    plt.grid(color='gray', linestyle='-', linewidth=0.5, alpha=0.2)
+    plt.plot(xValues, yValues, color='red', linestyle='-', linewidth=2, markersize=3)
     plt.xlim(0, samples)
     plt.ylim(1, 5)
     plt.xlabel('Number of Samples')
     plt.ylabel('Estimated Value of Pi')
-    plt.title('Monte Carlo Estimation of Pi')
+    plt.title('Monte Carlo Estimation of Pi', fontsize=14, fontweight='bold')
+    plt.axhline(y=math.pi, color='blue', linestyle='--', label='Actual value of Pi')
+    plt.legend()
     plt.show()
