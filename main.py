79aa1b22e8e27fe0e9526090274034bccede87ac
Pachrijam <paul830a@gmail.com>
2026-05-22 15:43:03 -0400

    intial commit #2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
-import matplotlib.pyplot as plt
-from calculatePI import monte_carlo_pi, percent_error
-from visualization import visualize_data
+from calculate import monte_carlo_pi
+from calculate import percent_error
 
 
-#Main
-print("How many samples to use for Monte Carlo estimation of Pi?")
+print("Estimating the value of pi using the Monte Carlo method.\nHow many random samples would you like to use?")
 num_samples = int(input())
-print("Estimated value of pi: {:.6f}".format(monte_carlo_pi(num_samples)))
-print("Simulation Percentage Error: {:.2f}%".format(percent_error(num_samples)))
+monte_carlo_result = monte_carlo_pi(num_samples)
+error_percentage = percent_error(num_samples)
+
+print("Estimated value of π: " + str(monte_carlo_result))
+print("Percent error: " + str(error_percentage) + "%")
--- visualization.py
+++
@@ -1,9 +0,0 @@
-import matpotlib.pyplot as plt
-def visualize_data(data):
-    plt.figure(figsize=(10, 6))
-    plt.plot(data['x'], data['y'], marker='o')
-    plt.title('Data Visualization')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.grid()
-    plt.show()
