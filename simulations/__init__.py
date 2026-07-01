3464571568b263ed07d654448afe7473df63d746
Paul Allen <paul830a@gmail.com>
2026-06-30 21:02:02 -0400

    feat: create simulations package structure

---
+++ simulations/__init__.py
@@ -0,0 +1,14 @@
+"""
+Monte Carlo Simulations Library
+
+This package contains various Monte Carlo simulation modules for:
+- Variance reduction techniques
+- Option pricing (European and path-dependent)
+- Portfolio risk metrics (VaR, ES)
+- MCMC methods for Bayesian inference
+- PDE and SDE solvers
+- Particle filters for state estimation
+- Rare event simulation
+"""
+
+__version__ = "1.0.0"
